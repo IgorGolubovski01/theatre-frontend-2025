506f056a5b3cd4e82ed07520ded5dcc3a0c2a85c
IgorGolubovski01 <igor.golubovski.21@singimail.rs>
2025-07-25 23:36:44 +0200

    Added register component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,13 +3,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
 
+export class RegisterComponent {
+  public username: string = ''
+  public email: string = ''
+  public password: string = ''
+
+
+  
 }
