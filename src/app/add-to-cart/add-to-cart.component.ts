d19c185ab8cafb0966911d962080717e78a87d81
IgorGolubovski01 <igor.golubovski.21@singimail.rs>
2025-07-27 12:46:59 +0200

    Added conponents

---
+++ src/app/add-to-cart/add-to-cart.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { ProjectionModel } from '../models/projection.model';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ProjectionService } from '../services/projection.service';
+import { MatTableModule } from '@angular/material/table';
+import { NgFor, NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { UserService } from '../services/user.service';
+
+@Component({
+  selector: 'app-add-to-cart',
+  imports: [MatTableModule, NgIf, MatButtonModule, MatCardModule, MatChipsModule, MatProgressBarModule, NgFor, RouterLink],
+  templateUrl: './add-to-cart.component.html',
+  styleUrl: './add-to-cart.component.css'
+})
+export class AddToCartComponent {
+  userService = UserService
+  displayedColumns: string[] = ['movie'];
+  dataSource: ProjectionModel[] | null = null
+
+  constructor(private route: ActivatedRoute) {
+    route.params.subscribe(params => {
+      ProjectionService.getProjectionById(params['id'])
+        .then(rsp => {
+          this.dataSource = [rsp.data]
+        })
+    })
+  }
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { ShowTimesComponent } from './show-times/show-times.component';
 import { DetailsComponent } from './details/details.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { PurchaseComponent } from './purchase/purchase.component';
+import { AddToCartComponent } from './add-to-cart/add-to-cart.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -12,5 +16,9 @@ export const routes: Routes = [
     { path: 'details/:id', component: DetailsComponent},
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
+    { path: 'contact', component: ContactComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'purchase', component: PurchaseComponent},
+    { path: 'add-to-cart/:id', component: AddToCartComponent},
     { path: '**', redirectTo: '' }
 ];
